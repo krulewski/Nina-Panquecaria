85fb1adcb5560e78c4449e02fd1c283d037fc112
krulewski <vinicius.delfino@fatec.sp.gov.br>
2024-10-24 15:32:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,17 +14,15 @@ document.getElementById('pedidoForm').addEventListener('submit', function(e) {
 
     // Capturar os dados do formulário
     const nome = document.getElementById('nome').value;
-    const endereco = document.getElementById('endereço').value;
-    const cidade = document.getElementById('cidade').value;
-    const email = document.getElementById('email').value;
     const telefone = document.getElementById('telefone').value;
     const sabor = document.querySelector('input[name="sabor"]:checked').value;
     const data = document.getElementById('data').value;
 
     // Montar a URL do WhatsApp com os dados
-    const msg = `Olá, gostaria de fazer um pedido:%0A%0ANome: ${nome}%0AEndereço: ${endereco}%0ACidade: ${cidade}%0AEmail: ${email}%0ATelefone: ${telefone}%0ASabor: ${sabor}%0AData e Hora de retirada: ${data}`;
+    const msg = `Olá, gostaria de fazer um pedido:%0A%0ANome: ${nome}%0ATelefone: ${telefone}%0ASabor: ${sabor}%0AData e Hora: ${data}`;
 
-    const whatsappUrl = `https://wa.me/5518996467780/?text=${msg}`;
+    // Substitua SEU_NUMERO_DE_TELEFONE pelo número correto (por exemplo: 5511999999999 para São Paulo, Brasil)
+    const whatsappUrl = `https://wa.me/5518996467780?text=${msg}`;
 
     // Redirecionar para o WhatsApp
     window.open(whatsappUrl, '_blank');
