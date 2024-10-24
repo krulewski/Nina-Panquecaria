9ab927b6b09569d3ae553bb9bfbe2d88e4e0a620
krulewski <vinicius.delfino@fatec.sp.gov.br>
2024-10-24 12:28:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ function menuShow() {
         document.querySelector('.icon').src = "img/close_white_36dp.svg";
     }
 }
+
+document.getElementById('pedidoForm').addEventListener('submit', function(e) {
+    e.preventDefault(); // Impede o envio do formulário
+
+    // Capturar os dados do formulário
+    const nome = document.getElementById('nome').value;
+    const endereco = document.getElementById('endereço').value;
+    const cidade = document.getElementById('cidade').value;
+    const email = document.getElementById('email').value;
+    const telefone = document.getElementById('telefone').value;
+    const sabor = document.querySelector('input[name="sabor"]:checked').value;
+    const data = document.getElementById('data').value;
+
+    // Montar a URL do WhatsApp com os dados
+    const msg = `Olá, gostaria de fazer um pedido:%0A%0ANome: ${nome}%0AEndereço: ${endereco}%0ACidade: ${cidade}%0AEmail: ${email}%0ATelefone: ${telefone}%0ASabor: ${sabor}%0AData e Hora de retirada: ${data}`;
+
+    const whatsappUrl = `https://wa.me/5518996467780/?text=${msg}`;
+
+    // Redirecionar para o WhatsApp
+    window.open(whatsappUrl, '_blank');
+});
